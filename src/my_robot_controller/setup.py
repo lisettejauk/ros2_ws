62302fbf7fe171926f8b462078c7bc46f8801033
lisettejauk <lisette.jauk@gmail.com>
2024-03-20 18:40:22 +0200

    Class work

---
+++ src/my_robot_controller/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'my_robot_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='autolab',
+    maintainer_email='kaldrekrister@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "test_node = my_robot_controller.my_first_node:main",
+            "draw_circle = my_robot_controller.draw_circle:main",
+            "mapping = my_robot_controller.mapping:main"
+        ],
+    },
+)
